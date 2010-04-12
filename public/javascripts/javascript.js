8012a1aef69c270aa7e280395fe32b77b5900eeb
darpanp <darpanp@gmail.com>
2010-04-12 01:17:04 -0400

    version-6 playing with jQuery

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
 	//hover effect for header
-	$("#home a").hover(
-		function(){$("#home_img").attr({src:"images/images/btn_home_over.jpg"})},
-		function(){$("#home_img").attr({src:"images/images/btn_home.jpg"})}
+	jQuery("#home a").hover(
+		function(){$("#home_img").attr({src:"../images/images/btn_home_over.jpg"})},
+		function(){$("#home_img").attr({src:"../images/images/btn_home.jpg"})}
 		);
-	$("#aboutus a").hover(
+	jQuery("#aboutus a").hover(
 		function(){$("#aboutus_img").attr({src:"images/images/btn_about_over.jpg"})},
 		function(){$("#aboutus_img").attr({src:"images/images/btn_about.jpg"})}
 		);
-	$("#contactus a").hover(
+	jQuery("#contactus a").hover(
 		function(){$("#contactus_img").attr({src:"images/images/btn_contacts_over.jpg"})},
 		function(){$("#contactus_img").attr({src:"images/images/btn_contacts.jpg"})}
 		);
