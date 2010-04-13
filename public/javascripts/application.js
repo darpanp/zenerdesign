5a5a20ba7718cc9f9e3e71c93cd2508061cbcd05
darpanp <darpanp@gmail.com>
2010-04-12 21:08:44 -0400

    version-7 fixed hover (also added 1.4.2 version of jQuery)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-jQuery.noConflict();
+$(document).ready(function() {
+	//hover effect for header
+	$("#home a").hover(
+                function(){$("#home_img").hide(); $("#home_img_hover").show();},
+                function(){$("#home_img").show(); $("#home_img_hover").hide();}
+                );
+	$("#aboutus a").hover(
+		function(){$("#aboutus_img").hide(); $("#aboutus_img_hover").show();},
+                function(){$("#aboutus_img").show(); $("#aboutus_img_hover").hide();}
+		);
+	$("#contactus a").hover(
+		function(){$("#contactus_img").hide(); $("#contactus_img_hover").show();},
+                function(){$("#contactus_img").show(); $("#contactus_img_hover").hide();}
+		);
+	
+	
+	
+	
+	
+	
+});
