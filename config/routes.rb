80bf01adb76e0c3a99cba75ea944edb968bb74c0
darpanp <darpanp@gmail.com>
2010-04-13 22:20:05 -0400

    ver-10 updated links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
-
+  map.homepage  '/homepage', :controller => 'pages', :action => 'homepage'
+  map.aboutus   '/aboutus', :controller => 'pages', :action => 'aboutus'
+  map.contact   '/contact', :controller => 'pages', :action => 'contact' 
+  
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
@@ -38,6 +41,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  map.root :controller => 'pages', :action => 'homepage'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
