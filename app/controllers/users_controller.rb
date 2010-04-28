aa7085bb92f06fa05b725c0213e2edbb80464cf8
darpanp <darpanp@gmail.com>
2010-04-28 00:04:14 -0400

    ver-19 removed flash from user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Zeiner Designs"
       redirect_to @user
     else
       render 'new'
